a776957761dfa90150ad0208b483f20ab77787ff
Axel Jesus Casique Mota <jesuscasiquem908@gmail.com>
2026-06-21 21:47:55 -0600

    definicion del metodo vender y while para las funciones

--- Retos/inventario.py
+++ Retos/inventario.py
@@ -27,4 +27,38 @@ def mostrar_inventario():
         for producto in inventario:
             print("Producto:", producto[0], "| Cantidad:", producto[1])
         print("------------------")
+def vender_producto():
+    nombre = input("Nombre del producto a vender: ")
 
+    encontrado = False
+    for producto in inventario:
+        if producto[0] == nombre:
+            inventario.remove(producto)
+            print("Producto vendido y retirado del inventario.")
+            encontrado = True
+            break
+
+    if encontrado == False:
+        print("Producto no encontrado.")
+
+# Programa principal
+while True:
+    print("\n--- Inventario Básico ---")
+    print("1. Agregar producto")
+    print("2. Mostrar inventario")
+    print("3. Vender producto")
+    print("4. Salir")
+
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        agregar_producto()
+    elif opcion == "2":
+        mostrar_inventario()
+    elif opcion == "3":
+        vender_producto()
+    elif opcion == "4":
+        print("Saliendo del sistema...")
+        break
+    else:
+        print("Opción no válida, intenta de nuevo.")
